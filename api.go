928f6a9fd016863533ef37b0807345f7d2753110
Sergio Velasco <serveba@prot-os.com>
2018-10-10 18:35:12 +0200

    IndexStudents, User API endpoints

--- api.go
+++ api.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"net/http/httputil"
@@ -40,13 +41,8 @@ type Exam struct {
 	Key ExamItem `json:"exam"`
 }
 
-// Users response of GET /USERS
-type Users struct {
-	Items []UserData `json:"users"`
-}
-
-// UserData data struct
-type UserData struct {
+// User internal data of user response
+type User struct {
 	ID            int64  `json:"id"`
 	Email         string `json:"email"`
 	Name          string `json:"name"`
@@ -55,7 +51,31 @@ type UserData struct {
 	InstituteName string `json:"institute_name"`
 }
 
-// Client sdk structure
+// UserWrapper json wrapper for get user response
+type UserWrapper struct {
+	Item User `json:"user"`
+}
+
+// Users response of GET /USERS
+type Users struct {
+	Items []User `json:"users"`
+}
+
+// Student response of GET /exams/id/show_student
+type Student struct {
+	ID     int64  `json:"id"`
+	Email  string `json:"email"`
+	Name   string `json:"name"`
+	Status string `json:"status"`
+	ExamID int64  `json:"exam_id"`
+}
+
+// Students reponse of GET /exams/:id/index_students
+type Students struct {
+	Items []Student `json:"students"`
+}
+
+// Client ProctorExam sdk metadata
 type Client struct {
 	BaseURL      *url.URL
 	httpClient   *http.Client
@@ -64,57 +84,8 @@ type Client struct {
 	apiSecretKey string
 }
 
-func (c *Client) newRequest(method, path string, body interface{}, params map[string]string) (*http.Request, error) {
-	rel := &url.URL{Path: path}
-	u := c.BaseURL.ResolveReference(rel)
-	var buf io.ReadWriter
-	if body != nil {
-		buf = new(bytes.Buffer)
-		err := json.NewEncoder(buf).Encode(body)
-		if err != nil {
-			return nil, err
-		}
-	}
-	signature := c.signParams(params)
-	target := fmt.Sprintf("%s?nonce=%s&timestamp=%s&signature=%s",
-		u.String(), params["nonce"], params["timestamp"], signature)
-
-	req, err := http.NewRequest(method, target, buf)
-	if err != nil {
-		return nil, err
-	}
-	if body != nil {
-		req.Header.Set("Content-Type", "application/json")
-	}
-	req.Header.Set("Accept", "application/vnd.procwise.v3")
-	req.Header.Set("User-Agent", c.UserAgent)
-	req.Header.Set("Authorization", "Token token="+c.apiKey)
-
-	return req, nil
-}
-
-func (c *Client) do(req *http.Request, v interface{}) (*http.Response, error) {
-	reqDump, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s", reqDump)
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// bodyBytes, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// bodyString := string(bodyBytes)
-	// fmt.Printf("RESPONSE: \n%s\n", bodyString)
-
-	err = json.NewDecoder(resp.Body).Decode(v)
-	return resp, err
+func (c *Client) newGetRequest(path string, params, queryParams map[string]string) (*http.Request, error) {
+	return c.newRequest("GET", path, nil, params, queryParams)
 }
 
 // same function as:
@@ -145,6 +116,67 @@ func (c *Client) signParams(params map[string]string) string {
 	return signature
 }
 
+func (c *Client) newRequest(method, path string, body interface{}, params, queryParams map[string]string) (*http.Request, error) {
+	rel := &url.URL{Path: path}
+	u := c.BaseURL.ResolveReference(rel)
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+	signature := c.signParams(params)
+
+	target := fmt.Sprintf("%s?nonce=%s&timestamp=%s&signature=%s",
+		u.String(), params["nonce"], params["timestamp"], signature)
+
+	if len(queryParams) > 0 {
+		for key, value := range queryParams {
+			target = fmt.Sprintf("%s&%s=%s", target, key, value)
+		}
+
+	}
+
+	req, err := http.NewRequest(method, target, buf)
+	if err != nil {
+		return nil, err
+	}
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	req.Header.Set("Accept", "application/vnd.procwise.v3")
+	req.Header.Set("User-Agent", c.UserAgent)
+	req.Header.Set("Authorization", "Token token="+c.apiKey)
+
+	return req, nil
+}
+
+func (c *Client) do(req *http.Request, v interface{}) (*http.Response, error) {
+	reqDump, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", reqDump)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	bodyString := string(bodyBytes)
+	fmt.Printf("RESPONSE: \n%s\n", bodyString)
+
+	err = json.NewDecoder(resp.Body).Decode(v)
+	return resp, err
+}
+
 func random(min, max int64) int64 {
 	rand.Seed(time.Now().Unix())
 	return rand.Int63n(max-min) + min
@@ -163,7 +195,7 @@ func getBaseParams() map[string]string {
 func (c *Client) Exams() (Exams, error) {
 	path := fmt.Sprintf("%s/exams", apiPrefix)
 	params := getBaseParams()
-	req, err := c.newRequest("GET", path, nil, params)
+	req, err := c.newGetRequest(path, params, nil)
 	if err != nil {
 		return Exams{}, err
 	}
@@ -172,36 +204,87 @@ func (c *Client) Exams() (Exams, error) {
 	return exams, err
 }
 
-// Exam method
-func (c *Client) Exam(id string) (*ExamItem, error) {
-	path := fmt.Sprintf("%s/exams/%s", apiPrefix, id)
+// Exam GET /exams/:id
+func (c *Client) Exam(id int64) (ExamItem, error) {
+	path := fmt.Sprintf("%s/exams/%d", apiPrefix, id)
 	params := getBaseParams()
-	params["id"] = id
-	req, err := c.newRequest("GET", path, nil, params)
+	params["id"] = strconv.Itoa(int(id))
+	req, err := c.newGetRequest(path, params, nil)
 	if err != nil {
-		return nil, err
+		return ExamItem{}, err
 	}
 	var exam Exam
 	_, err = c.do(req, &exam)
 
-	fmt.Printf("info: %v\n", exam)
-	return &exam.Key, err
+	return exam.Key, err
 }
 
-// Users returns a list of users
-func (c *Client) Users(instituteID string) ([]UserData, error) {
-	path := fmt.Sprintf("%s/institutes/%s/users", apiPrefix, instituteID)
+// Users GET /institutes/:institute_id/users
+func (c *Client) Users(instituteID int64) ([]User, error) {
+	path := fmt.Sprintf("%s/institutes/%d/users", apiPrefix, instituteID)
 	params := getBaseParams()
-	params["institute_id"] = instituteID
-	req, err := c.newRequest("GET", path, nil, params)
+	params["institute_id"] = strconv.Itoa(int(instituteID))
+	req, err := c.newGetRequest(path, params, nil)
 	if err != nil {
 		return nil, err
 	}
 	var users Users
 	_, err = c.do(req, &users)
 
-	fmt.Printf("Users: %v\n", users)
 	return users.Items, err
+}
+
+// ShowUser GET /institutes/:institute_id/users/:id
+func (c *Client) ShowUser(instituteID, userID int64) (User, error) {
+	path := fmt.Sprintf("%s/institutes/%d/users/%d", apiPrefix, instituteID, userID)
+	params := getBaseParams()
+	params["id"] = strconv.Itoa(int(userID))
+	params["institute_id"] = strconv.Itoa(int(instituteID))
+	req, err := c.newGetRequest(path, params, nil)
+	if err != nil {
+		return User{}, err
+	}
+	var user UserWrapper
+	_, err = c.do(req, &user)
+
+	return user.Item, err
+}
+
+// ShowStudent GET /exams/:id/show_student?student_session_id=
+func (c *Client) ShowStudent(examID, studentSessionID int64) (Student, error) {
+	path := fmt.Sprintf("%s/exams/%d/show_student", apiPrefix, examID)
+	params := getBaseParams()
+	sessionID := strconv.Itoa(int(studentSessionID))
+	key := "student_session_id"
+	params[key] = sessionID
+	req, err := c.newGetRequest(path, params, map[string]string{key: sessionID})
+	// req, err := c.newGetRequest(path, params, nil)
+	if err != nil {
+		return Student{}, err
+	}
+	var student Student
+	_, err = c.do(req, &student)
+
+	return student, err
+}
+
+// IndexStudents GET /exams/:id/index_students
+func (c *Client) IndexStudents(examID int64) ([]Student, error) {
+	path := fmt.Sprintf("%s/exams/%d/index_students", apiPrefix, examID)
+	params := getBaseParams()
+	params["id"] = strconv.Itoa(int(examID))
+
+	fmt.Printf("%v\n", params)
+	req, err := c.newGetRequest(path, params, nil)
+
+	if err != nil {
+		return nil, err
+	}
+	var students Students
+	_, err = c.do(req, &students)
+
+	fmt.Printf("Users: %v\n", students)
+	return students.Items, nil
 }
 
 func main() {
@@ -222,19 +305,20 @@ func main() {
 		os.Getenv("PE_API_KEY"), os.Getenv("PE_SECRET_KEY"))
 
 	// exams, err := c.Exams()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("exams: %v\n", exams)
+	// fmt.Printf("exams: %v\n", *exams)
 
-	users, err := c.Users("1")
+	// users, err := c.Users(1)
+	// fmt.Printf("users: %v\n", users)
 
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("users: %v\n", users)
+	// user, err := c.ShowUser(1, 11)
+	// fmt.Printf("user: %v\n", user)
 
-	// exam, err := c.Exam("17")
+	// exam, err := c.Exam(70)
+	// fmt.Printf("exam: %v\n", *exam)
 
-	// fmt.Printf("exam: %v\n", exam)
+	// students, err := c.IndexStudents(70)
+	// fmt.Printf("students: %v\n", *students)
+
+	student, err := c.ShowStudent(70, 804)
+	fmt.Printf("student: %v\n", student)
 }
